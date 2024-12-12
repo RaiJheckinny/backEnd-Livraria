cb839f0360b4396c1d0969c91d4f2b2a67d27d19
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-11 23:26:14 -0300

    feat:create varivel price for book

--- src/main/java/com/livraria/domain/Book/Book.java
+++ src/main/java/com/livraria/domain/Book/Book.java
@@ -8,11 +8,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -26,13 +21,75 @@ public class Book {
     private String author;
     @Column(nullable = false)
     private LocalDateTime datePublication;
-    @Column(length = 100, nullable = false)
-    private Double stockQuantity;
+    @Column(nullable = false)
+    private Long stockQuantity;
+    @Column(nullable = false)
+    private Double price;
 
     public Book(CreateBookDTO createBookDTO){
         this.name = createBookDTO.name();
         this.description = createBookDTO.description();
         this.author = createBookDTO.author();
         this.stockQuantity = createBookDTO.stockQuantity();
+        this.price = createBookDTO.price();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getDatePublication() {
+        return datePublication;
+    }
+
+    public void setDatePublication(LocalDateTime datePublication) {
+        this.datePublication = datePublication;
+    }
+
+    public Long getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Long stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Book(){
     }
 }
