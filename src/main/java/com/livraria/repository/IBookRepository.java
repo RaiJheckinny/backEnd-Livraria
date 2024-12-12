a233f9c0ee6eec3c54e42aa8d210a1f877b00cba
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-11 22:27:43 -0300

    feat:create Book repository

---
+++ src/main/java/com/livraria/repository/IBookRepository.java
@@ -0,0 +1,12 @@
+package com.livraria.repository;
+
+import com.livraria.domain.Book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public interface IBookRepository extends JpaRepository<Book, UUID> {
+    Optional<Book> findByUUID(Long id);
+}
