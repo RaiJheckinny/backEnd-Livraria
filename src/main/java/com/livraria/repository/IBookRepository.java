c98d6f29b52ae84abe4a73f9eecd96c1aa2bc9b0
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-12 23:14:39 -0300

    fix: book repository

--- src/main/java/com/livraria/repository/IBookRepository.java
+++ src/main/java/com/livraria/repository/IBookRepository.java
@@ -1,6 +1,6 @@
 package com.livraria.repository;
 
-import com.livraria.domain.Book.Book;
+import com.livraria.domain.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
